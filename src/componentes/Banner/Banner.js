15a03f26f695b76bf402f48ddc5c57b4b14844cb
Dener Patrick <dener@outlook.com>
2024-06-26 20:51:08 -0300

    Finalizando o curso

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,11 @@
+import  './Banner.css'
+
+function Banner () {
+    return (
+        <header className='banner'>
+            <img src="/imagens/banner.png" alt="Banner principal com o título: Pessoas e times e subtitulo subtítulo: organizados em um só lugar! e Logo Organo"/>
+        </header>
+    )
+}
+
+export default Banner
